072dba5439ee40e61fdb7ceca57576e557fb7ad6
nlwz <122232821+n1wz@users.noreply.github.com>
2023-02-19 19:28:06 +0300

    Add files via upload

---
+++ readme.md
@@ -0,0 +1,7 @@
+# YaraScan
+
+## Installation
+Before using, you must install Yara python plugin with ``pip install yara-python``
+
+## Using
+Press hot-key combination ``Shift-Y`` and select your Yara rules directory.
---
+++ yarascan.py
@@ -0,0 +1,92 @@
+import ida_kernwin
+import ida_segment
+import ida_bytes
+import ida_idaapi
+import idc
+import yara
+import os
+
+class YaraSettings(ida_kernwin.Form):
+    def __init__(self):
+        ida_kernwin.Form.__init__(self, r"""BUTTON YES* Search
+BUTTON CANCEL Cancel
+YaraScan
+
+<##Yara Rules directory:{lPath}>""", { "lPath":  ida_kernwin.Form.DirInput() })
+        self.Compile()
+
+    def Show(self):
+        settings_path = f"{idc.idadir()}\\yara.settings"
+        if os.path.isfile(settings_path):
+            with open(settings_path, "r") as settings:
+                self.lPath.value = settings.read()
+        out = self.Execute()
+        if out == 1:
+            with open(settings_path, "w") as settings:
+                settings.write(self.lPath.value)
+        return [ out, self.lPath.value ]
+
+class YaraChoose(ida_kernwin.Choose):
+    def __init__(self, title, vals):
+        ida_kernwin.Choose.__init__(self, title, [
+            ["Address", ida_kernwin.Choose.CHCOL_HEX | 10 ],
+            ["Rule Name", ida_kernwin.Choose.CHCOL_PLAIN | 20 ],
+            ["Description", ida_kernwin.Choose.CHCOL_PLAIN | 30 ],
+            ["Filename", ida_kernwin.Choose.CHCOL_PLAIN | 15 ],
+            ["Signature", ida_kernwin.Choose.CHCOL_PLAIN | 30 ],
+        ])
+        self.vals = vals
+
+    def OnGetSize(self):
+        return len(self.vals)
+
+    def OnGetLine(self, n):
+        return [ f"0x{hex(self.vals[n][0]).upper()[2:]}", self.vals[n][1], self.vals[n][2], self.vals[n][3], self.vals[n][4]]
+
+    def OnSelectLine(self, sel):
+        ida_kernwin.jumpto(self.vals[sel][0])
+
+def GetSections():
+    sec, sec_m = ida_segment.get_first_seg(), []
+    for i in range(5):
+        if sec == None:
+            break
+        sec_m.append(sec)
+        sec = ida_segment.get_next_seg(sec.start_ea)
+    return sec_m
+
+def YaraScan(rule_file):
+    rule, matches = yara.compile(file = open(rule_file)), []
+    for sec in GetSections():
+        bytes = ida_bytes.get_bytes(sec.start_ea, sec.end_ea - sec.start_ea)
+        for r in rule.match(data = bytes):
+            for d in r.strings:
+                matches.append([ sec.start_ea + d[0], r.rule, r.meta["description"] if "description" in r.meta else "", rule_file[rule_file.rfind("\\") + 1:], str(d[2])])
+                ida_bytes.set_cmt(sec.start_ea + d[0], r.rule + " / " + (r.meta["description"] if "description" in r.meta else ""), True)
+    return matches
+
+class YaraIDA(ida_idaapi.plugin_t):
+    comment = ""
+    help = ""
+    wanted_name = "YaraScan"
+    wanted_hotkey = "Shift-Y"
+    flags = ida_idaapi.PLUGIN_KEEP
+
+    def init(self):
+        return ida_idaapi.PLUGIN_KEEP
+
+    def term(self):
+        pass
+
+    def run(self, arg):
+        res = YaraSettings().Show()
+        if res[0] == 1:
+            matches = []
+            for file in os.listdir(res[1]):
+                if file[-4:] == ".yar":
+                    matches_l = YaraScan(res[1] + "\\" + file)
+                    matches += matches_l
+            c = YaraChoose("Yara Detections", matches).Show()
+
+def PLUGIN_ENTRY():
+    return YaraIDA()
